6242e213969f4d24112479babe4c58a29c2985d2
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-04-08 14:01:42 +0700

    buat page mentor detail

---
+++ client/src/pages/mentorDetail.jsx
@@ -0,0 +1,117 @@
+// import PropTypes from "prop-types";
+
+const MentorDetail = () => {
+  // object mentors
+  // const mentors = [
+  //   {
+  //     id: 1,
+  //     work: "Alex Freshman",
+  //     email: "Head of Phaselleus Vitae",
+  //     linkedin:"https://www.linkedin.com/in/muhammad-abdanul-ikhlas/",
+  //     location: "United States of Majalengka",
+  //     rating: 5.0,
+  //     reviewers:196,
+  //     skills: [
+  //       'Enterpreneurship',
+  //       'Marketing',
+  //       'Branding'
+  //     ],
+  //     about:"Alex Freshman is an exceptional mentor who brings a wealth of experience  and enthusiasm to the mentoring program. With a solid background in mention relevant field or industry, Alex demonstrates a genuine  passion for guiding and supporting mentees in their personal and  professional development.",
+  //     experiences: [
+  //       {year: 2019, desc: 'Sunt nostrud amet sint do'},
+  //       {year: 2020, desc: 'Exercitation veniam consequat'},
+  //       {year: 2021, desc: 'Velit officia consequat duis'},
+  //     ],
+  //     mentees:[1,2,3],
+  //   }
+  // ];
+
+  // // object mentee overview
+  // const menteesOverview = [
+  //   {
+  //     id: 1,
+  //     name:"Alex Jr.",
+  //     image:"../images/mentee-profile-1.png",
+  //     rating:5.0,
+  //     comment:"“Saya telah menggunakan platform Mentor Kita selama beberapa bulan, dan  pengalaman saya sungguh luar biasa. Sebagai seorang profesional yang  ingin terus berkembang, saya mencari sumber pembelajaran yang tidak  hanya informatif tetapi juga interaktif. Mentor Kita memberikan lebih  dari yang saya harapkan.",
+  //   },
+  //   {
+  //     id: 2,
+  //     name:"David Jr.",
+  //     image:"../images/mentee-profile-2.png",
+  //     rating:5.0,
+  //     comment:"“Saya telah menggunakan platform Mentor Kita selama beberapa bulan, dan  pengalaman saya sungguh luar biasa. Sebagai seorang profesional yang  ingin terus berkembang, saya mencari sumber pembelajaran yang tidak  hanya informatif tetapi juga interaktif. Mentor Kita memberikan lebih  dari yang saya harapkan.",
+  //   },
+  //   {
+  //     id: 3,
+  //     name:"Jesse Jr.",
+  //     image:"../images/mentee-profile-3.png",
+  //     rating:5.0,
+  //     comment:"Saya telah menggunakan platform Mentor Kita selama beberapa bulan, dan  pengalaman saya sungguh luar biasa. Sebagai seorang profesional yang  ingin terus berkembang, saya mencari sumber pembelajaran yang tidak  hanya informatif tetapi juga interaktif. Mentor Kita memberikan lebih  dari yang saya harapkan.",
+  //   }
+  // ];
+
+  return (
+    <>
+      <header className="w-full h-[67px] bg-white">rawr</header>
+      <main className="font-poppins bg-white">
+        test
+      </main>
+    </>
+  );
+};
+
+// const CardKonsultasi = ({ name, email, question, date, time }) => {
+//   CardKonsultasi.propTypes = {
+//     name: PropTypes.string,
+//     email: PropTypes.string,
+//     question: PropTypes.string,
+//     date: PropTypes.string,
+//     time: PropTypes.string,
+//   };
+
+//   return (
+//     <section className="font-body flex flex-col gap-0">
+//       {/* nama */}
+//       <div className="flex">
+//         <div className="w-[43%] box-border text-sm border border-primary bg-primary p-4 text-white ">
+//           Nama
+//         </div>
+//         <div className="w-[60%] box-border text-sm bg-white p-4 text-black ">
+//           {name}
+//         </div>
+//       </div>
+//       {/* email */}
+//       <div className="flex">
+//         <div className="w-[43%] box-border text-sm border border-primary bg-primary p-4 text-white ">
+//           Email
+//         </div>
+//         <div className="w-[60%] box-border text-sm bg-white p-4 text-black ">
+//           {email}
+//         </div>
+//       </div>
+//       {/* Pertanyaan */}
+//       <div className="flex">
+//         <div className="w-[43%] box-border text-sm border border-primary bg-primary p-4 text-white ">
+//           Pertanyaan
+//         </div>
+//         <div className="w-[60%] box-border text-sm bg-white p-4 text-black ">
+//           {question}
+//         </div>
+//       </div>
+//       {/* Waktu Pengisian */}
+//       <div className="flex">
+//         <div className="w-[43%] box-border text-sm border border-primary bg-primary p-4 text-white ">
+//           Waktu Pengisian
+//         </div>
+//         <div className="w-[60%] box-border text-sm bg-white p-4 text-black ">
+//           {date} <br />
+//           {time}
+//         </div>
+//       </div>
+//       <div className="w-full h-1 bg-[#E9E9E9]"></div>
+//     </section>
+//   );
+// };
+
+export default MentorDetail;
