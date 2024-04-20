96d807002417e8b1817c38a07cc388d61e8d1028
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-04-20 09:01:42 +0700

    membuat routing untuk link antar page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Dashboard from './pages/Dashboard'
+import MentorDetail from './pages/mentorDetail'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/mentorDetail/:id",
+    element: <MentorDetail />,
+  },
+  // tambahkan path nya di bawah sini, contohnya ini, jangan lupa import page nya
+  // {
+  //   path: "/searchMentor",
+  //   element: <SearchMentor />,
+  // },
+  // {
+  //   path: "/beAMentor",
+  //   element: <BeAMmentor />,
+  // },
+  // {
+  //   path: "/login",
+  //   element: <Login />,
+  // },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Dashboard />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
