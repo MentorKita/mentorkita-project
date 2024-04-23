b0d41cc6d781f3e1c07d071f67998aead853915c
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-04-24 00:23:34 +0700

    rename file menteesOverview menjadi mentees.js dan update file nya

---
+++ client/src/data/mentees.js
@@ -0,0 +1,54 @@
+const mentees = [
+  {
+    id: 1,
+    name: "Mike",
+    image: "../../../images/mentor-photo-1.png",
+    rating: 5.0,
+    comment:
+      "Saya telah menggunakan platform Mentor Kita selama beberapa bulan, dan  pengalaman saya sungguh luar biasa. Sebagai seorang profesional yang  ingin terus berkembang, saya mencari sumber pembelajaran yang tidak  hanya informatif tetapi juga interaktif. Mentor Kita memberikan lebih  dari yang saya harapkan.",
+    status: "Waiting",
+  },
+  {
+    id: 2,
+    name: "Mikael",
+    image: "../../../images/mentor-photo-5.png",
+    rating: 4.0,
+    comment:
+      "Saya telah menggunakan platform Mentor Kita selama beberapa bulan, dan  pengalaman saya sungguh luar biasa. Sebagai seorang profesional yang  ingin terus berkembang, saya mencari sumber pembelajaran yang tidak  hanya informatif tetapi juga interaktif. Mentor Kita memberikan lebih  dari yang saya harapkan.",
+    status: "On Progress",
+  },
+  {
+    id: 3,
+    name: "Jesse Jr.",
+    image: "../../../images/mentor-photo-4.png",
+    rating: 5.0,
+    comment:
+      "Saya telah menggunakan platform Mentor Kita selama beberapa bulan, dan  pengalaman saya sungguh luar biasa. Sebagai seorang profesional yang  ingin terus berkembang, saya mencari sumber pembelajaran yang tidak  hanya informatif tetapi juga interaktif. Mentor Kita memberikan lebih  dari yang saya harapkan.",
+    status: "Done",
+  },
+];
+
+const alumniMentees = [
+  {
+    id: 1,
+    name: "Marry Juanna",
+    alumniCourse: "Web Development Mentee",
+    image: "../../../images/mentor-photo-3.png",
+    rating: 5.0,
+    comment:
+      "Saya telah menggunakan platform Mentor Kita selama beberapa bulan, dan  pengalaman saya sungguh luar biasa. Sebagai seorang profesional yang  ingin terus berkembang, saya mencari sumber pembelajaran yang tidak  hanya informatif tetapi juga interaktif. Mentor Kita memberikan lebih  dari yang saya harapkan.",
+    status: "Waiting",
+  },
+  {
+    id: 2,
+    name: "Anntony",
+    alumniCourse: "Web Development Mentee",
+    image: "../../../images/mentor-photo-2.png",
+    rating: 4.0,
+    comment:
+      "Saya telah menggunakan platform Mentor Kita selama beberapa bulan, dan  pengalaman saya sungguh luar biasa. Sebagai seorang profesional yang  ingin terus berkembang, saya mencari sumber pembelajaran yang tidak  hanya informatif tetapi juga interaktif. Mentor Kita memberikan lebih  dari yang saya harapkan.",
+    status: "On Progress",
+  },
+];
+
+export { mentees, alumniMentees };
