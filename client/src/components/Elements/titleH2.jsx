4cfbe60be21eb88c62d4842ddf9311d40634acad
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-04-24 00:24:24 +0700

    update komponen titleH2 dan cardMentee

--- client/src/components/Elements/titleH2.jsx
+++ client/src/components/Elements/titleH2.jsx
@@ -5,7 +5,7 @@ const TitleH2 = ({ title }) => {
   };
   return (
     <div className="">
-      <h2 className="font-medium text-[#020721] mt-5 mb-2">{title}</h2>
+      <h2 className="font-medium text-[#020721] mt-5 mb-2 text-base">{title}</h2>
     </div>
   );
 };
