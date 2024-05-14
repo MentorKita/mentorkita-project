3bb6b6734f6ce40d7b0a1ea649b93369c6861b69
Hariel Dananjaya <igedeharieldananjaya026@gmail.com>
2024-05-14 17:36:51 +0700

    Update baru

--- client/src/components/Fragments/Navbar.jsx
+++ client/src/components/Fragments/Navbar.jsx
@@ -10,17 +10,17 @@ function Navbar() {
 
   return (
     <>
-      <div className="bg-white flex flex-row h-[50px] bg-gradient-to-r from-[#081C87] to-[#27B2DD] fixed top-0 w-full z-20 lg:w-[1280px] lg:h-[67px] ">
-        <p className="text-black py-[15px] px-[15px] mr-[270px] lg:px-0 lg:z-30 lg:pt-[20px] lg:mr-[245px]"><img className="size-5 lg:w-[32px] lg:h-[24px] lg:border lg:border-black lg:ml-[25px] " src="../../../public/images/Logo.png" alt="" /></p>
-        <ul className="lg:flex lg:flex-row sm:hidden lg:w-[700px] lg:ml-[300px]">
-            <li className="lg:text-white lg:font-semibold lg:w-[200px] lg:text-[20px] lg:leading-[30px] lg:pt-[15px] lg:text-center "><Link to="/"> Dashboard </Link></li>
-            <li className="lg:text-white lg:font-semibold lg:w-[200px] lg:text-[20px] lg:leading-[30px] lg:pt-[15px] lg:text-center "><Link to="/searchMentor"> Search Mentors </Link></li>
-            <li className="lg:text-white lg:font-semibold lg:w-[200px] lg:text-[20px] lg:leading-[30px] lg:pt-[15px] lg:text-center "><Link to="/becomeAMentor"> Be a Mentor </Link></li>
-            <li className="lg:text-white lg:font-semibold lg:w-[150px] lg:text-[20px] lg:leading-[30px] lg:pt-[15px] lg:text-center "><Link to="/loginUser"> Log In</Link></li>
+      <div className="bg-white flex flex-row h-[50px] bg-gradient-to-r from-[#081C87] to-[#27B2DD] fixed top-0 w-full z-20 lg:h-[67px] md:h-[60px] ">
+        <p className="text-black py-[15px] px-[15px] mr-auto lg:px-0 lg:z-30 lg:pt-[20px] lg:mr-[245px]"><img className="size-5 lg:w-[32px] lg:h-[24px] lg:border lg:border-black lg:ml-[25px] " src="../../../public/images/Logo.png" alt="" /></p>
+        <ul className="flex md:flex-row lg:flex-row lg:w-[700px] lg:ml-[300px]">
+            <li className="hidden md:block lg:block lg:text-white lg:font-semibold lg:w-[200px] lg:text-[20px] lg:leading-[30px] lg:pt-[15px] lg:text-center md:text-white md:font-semibold md:w-[150px] md:text-[20px] md:leading-[30px] md:pt-[15px] md:text-center "><Link to="/"> Dashboard </Link></li>
+            <li className="hidden md:block lg:block lg:text-white lg:font-semibold lg:w-[200px] lg:text-[20px] lg:leading-[30px] lg:pt-[15px] lg:text-center md:text-white md:font-semibold md:w-[150px] md:text-[20px] md:leading-[30px] md:pt-[15px] md:text-center"><Link to="/searchMentor"> Search Mentors </Link></li>
+            <li className="hidden md:block lg:block lg:text-white lg:font-semibold lg:w-[200px] lg:text-[20px] lg:leading-[30px] lg:pt-[15px] lg:text-center md:text-white md:font-semibold md:w-[150px] md:text-[20px] md:leading-[30px] md:pt-[15px] md:text-center"><Link to="/becomeAMentor"> Be a Mentor </Link></li>
+            <li className="hidden md:block lg:block lg:text-white lg:font-semibold lg:w-[150px] lg:text-[20px] lg:leading-[30px] lg:pt-[15px] lg:text-center md:text-white md:font-semibold md:w-[100px] md:text-[20px] md:leading-[30px] md:pt-[15px] md:text-center"><Link to="/loginUser"> Log In</Link></li>
           </ul>
-        <button onClick={toggleMenu} className="block lg:ml-[910px] "><p className="text-black p-[10px] "><img className="size-5 p-[3px] bg-white rounded" src="../../../public/images/Vector.png" alt="" /></p></button>
+        <button onClick={toggleMenu} className="block lg:ml-[910px] md:hidden mr-6 "><p className="text-black p-[10px] "><img className="size-5 p-[3px] bg-white rounded" src="../../../public/images/Vector.png" alt="" /></p></button>
       </div>
-      <div className={`${isOpen ? 'block' : 'hidden'}  w-[180px] h-[400px] bg-white ml-[195px] border-2 rounded-lg fixed z-20 lg:ml-[1100px] lg:hidden `} >
+      <div className={`${isOpen ? 'block' : 'hidden'}  w-[180px] h-[400px] bg-white ml-48 border-2 rounded-lg fixed z-20 lg:ml-[1100px] md:hidden`} >
         <nav className="w-[180px] h-[170px]">
           <ul className="flex flex-col">
             <li className="text-black text-base px-[40px] py-[30px] font-semibold mx-auto"><a href="#"> <img className="mt-7 mx-auto w-[90px] bg-gray-300 rounded-full" src="https://cdn1.iconfinder.com/data/icons/basic-ui-set-v5-user-outline/64/Account_profile_user_avatar_rounded-512.png" alt="" /></a></li>
