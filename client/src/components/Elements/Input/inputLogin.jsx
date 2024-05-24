4028a7af7661c6e4c1679f21698e69c6a59e9232
Hariel Dananjaya <igedeharieldananjaya026@gmail.com>
2024-05-24 17:44:26 +0700

    update form

--- client/src/components/Elements/Input/inputLogin.jsx
+++ client/src/components/Elements/Input/inputLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function InputLogin({ placeholder }) {
+function InputLogin({type,  placeholder }) {
   const [email, setEmail] = useState('');
 
   const handleInputChange = (event) => {
@@ -9,8 +9,7 @@ function InputLogin({ placeholder }) {
 
   return (
     <div className="flex flex-col">
-      <input type="email" id="email" value={email} onChange={handleInputChange} className="w-[350px] h-[60px] mx-auto bg-[#ECECEC] rounded-lg px-3 py-2 mt-1 lg:w-[464px] lg:h-[62px] lg:py-[10px] lg:px-[22px]" placeholder={placeholder} required
-      />
+      <input type={type} value={email} onChange={handleInputChange} className="w-[350px] h-[60px] mx-auto bg-[#ECECEC] rounded-lg px-3 py-2 mt-1 lg:w-[464px] lg:h-[62px] lg:py-[10px] lg:px-[22px]" placeholder={placeholder} required/>
     </div>
   );
 }
